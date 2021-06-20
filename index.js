380da78337666266cd17109db5801cff369d63c8
Alasakani Pranitha <73059159+pranithaalasakani123@users.noreply.github.com>
2021-06-20 21:49:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function generation(){
+   var image = document.createElement('img');
+   var div = document.getElementById('flex-cat-gen');
+   image.src = "https://tse1.mm.bing.net/th?id=OIP.Wj84VoOW51ukmu4VqjwCcQHaFj&pid=Api&P=0&w=202&h=152";
+   div.appendChild(image);
+console.log(image);
+}
